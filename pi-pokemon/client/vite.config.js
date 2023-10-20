fc7c11f0edfe29d33387450853934e66b4875837
Jose Lozano <joselozano.dev@gmail.com>
2023-10-20 17:34:28 -0400

    config gh pages

--- pi-pokemon/client/vite.config.js
+++ pi-pokemon/client/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './client/src/main.jsx'
-    }
-  }
+  base: "/PokeApp/"
 })
