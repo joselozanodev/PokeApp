424e98dc030fe73cccdc58c217fdca7725bfa74c
Jose Lozano <joselozano.dev@gmail.com>
2023-10-20 18:13:14 -0400

    modification

--- pi-pokemon/client/src/components/LandingPage/Landing.jsx
+++ pi-pokemon/client/src/components/LandingPage/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Landing.css';
+import './landing.css';
 
 const Landing = ()=>{
     const navigate = useNavigate();
